0038e1a944f8fcefea89cf23f6b3dfbc7a1651dc
Pawel Warmuth <warmuth.pawel@gmail.com>
2022-08-04 21:05:27 +0200

    Added save item logic

--- backend/lambda/getUploadUrl/app.ts
+++ backend/lambda/getUploadUrl/app.ts
@@ -1,9 +1,16 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
-import { S3RequestPresigner, getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3'
+import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb"
 
 interface GetUploadUrlResponse {
     urls: { fileName: string, uploadUrl: string }[]
+}
+
+interface GetUploadUrlRequest{
+    files:string[],
+    album:string
 }
 
 /**
@@ -20,6 +27,8 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
     let userSub = event.requestContext.authorizer?.jwt.claims.sub;
     let userMail = event.requestContext.authorizer?.jwt.claims.email;
     let s3Client = new S3Client({});
+    let ddbDocumentCLient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+    let tableName = process.env.TABLE_NAME || "data";
 
     if (userSub == null) {
         return {
@@ -35,12 +44,23 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
         }
     };
 
-    let files: string[] = [...event.body];
+    let body:GetUploadUrlRequest
+    try {
+        body = JSON.parse(event.body);
+    } catch (error) {
+        console.error(`Error while parsing body: ${event.body} with error: ${error}`)
+        return {
+            body: JSON.stringify(error),
+            statusCode:500
+        }
+    }
+     
+    let files: string[] = [...body.files];
     let getUploadUrlResonse: GetUploadUrlResponse = { urls: [] };
     for (let file of files) {
         let signedUrl = await await getSignedUrl(s3Client, new GetObjectCommand({ Bucket: userSub, Key: file }), { expiresIn: 12600 });
         try {
-            saveSignedUrlRequest(signedUrl, file, userMail);
+            saveSignedUrlRequest(ddbDocumentCLient,tableName,signedUrl, file, userSub,body.album,userMail);
         } catch (error) {
             console.error(error);
             return {
@@ -57,8 +77,16 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
     }
 };
 
-const saveSignedUrlRequest = (signedUrl: string, fileKey: string, userMail: string) => {
-
+const saveSignedUrlRequest = async (dbClient:DynamoDBDocumentClient,
+    tableName:string, signedUrl: string, fileKey: string, sub: string,album:string,email:string) => {
+    await dbClient.send(new PutCommand({
+        TableName:tableName,
+        Item: {
+            PK: `FILE#${fileKey}_${sub}`,
+            SK: `FILE#${fileKey}_${sub}`,
+            GSI1PK:`USER#${email}ALBUM#${album}`,
+            GSI1SK:`FILE#${fileKey}_${sub}`,
+            uploadUrl:signedUrl
+        }
+    }));
 }
-
-
