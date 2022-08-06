d903c8c9dbf70bad65e8d459e7b60db39d71bd8d
Pawel Warmuth <warmuth.pawel@gmail.com>
2022-08-06 20:57:48 +0200

    Added Draft Album

---
+++ frontend/src/Album/Album.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Album from './AlbumItem.svelte'
+ </script>
+
+<style>
+	#albums {
+		display:flex;
+		margin-top: 2%;
+		flex-wrap: wrap;
+	}
+</style>
+
+<div id="albums" class="container-fluid album py-5 bg-light ">
+	<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+		<Album></Album>
+		<Album></Album>
+		<Album></Album>
+		<Album></Album>
+		<Album></Album>
+		<Album></Album>
+		<Album></Album>
+		<Album></Album>
+		<Album></Album>
+		<Album></Album>
+		<Album></Album>
+		<Album></Album>
+	</div>
+</div>
---
+++ frontend/src/Album/AlbumItem.svelte
@@ -0,0 +1,31 @@
+<script>
+
+</script>
+
+<style>
+    img {
+        width: 280px;
+        height: 250px;
+        position: relative;
+        margin: 1px;
+    }
+    .card {
+        width: fit-content;
+        margin-left: 2%;
+        margin-bottom: 2%;
+    }
+    .card-img-overlay {
+        position:absolute;
+        bottom: 5%;
+        width: 100%;
+        color: white;
+    }
+</style>
+
+<div class="card img-fluid shadow-sm">
+    <img class="card-img-top" src="https://live.staticflickr.com/65535/52261060158_bdb6c0b904_c.jpg" alt="Hamburg2022"/>
+    <div class="card-img-overlay">
+        <h4 class="card-title">Hamburg 2022</h4>
+        <p class="card-text">138 Fotos - 4 Aufrufe</p>
+      </div>
+</div>
