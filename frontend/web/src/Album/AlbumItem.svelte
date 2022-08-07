8c3980c615eca381324ef27452d35b43972f2e82
Pawel Warmuth <warmuth.pawel@gmail.com>
2022-08-07 10:38:50 +0200

    Changed naming and switched to vite

---
+++ frontend/web/src/Album/AlbumItem.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    export let title:string;
+    export let views:string;
+</script>
+
+<div class="card bg-dark text-white">
+    <img
+        class="card-img"
+        src="https://live.staticflickr.com/65535/52261060158_bdb6c0b904_c.jpg"
+        alt="Hamburg2022"
+    />
+    <div class="card-img-overlay position-absolute">
+        <h4 class="card-title">{title}</h4>
+        <p class="card-text">{views} Fotos - {views} Aufrufe</p>
+    </div>
+</div>
+
+<style>
+    img {
+        width: 280px;
+        height: 250px;
+    }
+    .card {
+        width: fit-content;
+        margin-left: 2%;
+        margin-bottom: 2%;
+        border: none;
+    }
+</style>
--- frontend/web/src/App.svelte
+++ frontend/web/src/App.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
    import router from "page";
    import routes from "./routes";
 
