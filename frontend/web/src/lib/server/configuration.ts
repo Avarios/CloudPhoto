f6df1056a429adfc9ef772cf4f42839cd14445ad
Pawel Warmuth <warmuth.pawel@gmail.com>
2022-12-22 20:28:49 +0100

    removd keys

--- frontend/web/src/lib/server/configuration.ts
+++ frontend/web/src/lib/server/configuration.ts
@@ -1,3 +1,3 @@
 export const COGNITO_OAUTH2_URL = 'https://cloudphoto.auth.eu-central-1.amazoncognito.com/oauth2'
-export const COGNITO_CLIENTID = '489sprphn6v08t0822odoj5e1g'
-export const COGNITO_CLIENTSECRET = 'qshpe6lb8h1fv04pd6ic6gslnva28asu66dv0l8s9t2br3lvirc'
+export const COGNITO_CLIENTID = ''
+export const COGNITO_CLIENTSECRET = ''
