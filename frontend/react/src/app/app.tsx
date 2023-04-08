ce33febe3c36e9a88ebaafd0923dab839972aeb7
Pawel Warmuth <warmuth.pawel@gmail.com>
2023-04-08 22:35:49 +0200

    Added Icons

--- frontend/react/src/app/app.tsx
+++ frontend/react/src/app/app.tsx
@@ -1,13 +1,36 @@
-
-import { TopBar } from './components/appbar'
+import { TopBar } from './components/appbar';
 import { Main } from './components/home';
+import {
+  Grid,
+  View,
+  ThemeProvider,
+  defaultDarkModeOverride,
+} from '@aws-amplify/ui-react';
 
 export function App() {
   return (
-    <div>
-      <TopBar></TopBar>
-      <Main></Main>
-    </div>
+    <ThemeProvider
+      colorMode="system"
+      theme={{
+        name: 'cloudphotoTheme',
+        overrides: [defaultDarkModeOverride],
+      }}
+    >
+      <div className='container'>
+        <Grid
+          templateColumns="1fr 1fr 1fr"
+          gap={'small'}
+          rowGap={'xxs'}
+        >
+          <View columnSpan={3}>
+            <TopBar></TopBar>
+          </View>
+          <View>
+            <Main></Main>
+          </View>
+        </Grid>
+      </div>
+    </ThemeProvider>
   );
 }
 
