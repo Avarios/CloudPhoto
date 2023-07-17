3d72da9e2e3b11aef1a19bde146e8c16cc9bf760
Pawel Warmuth <warmuth.pawel@gmail.com>
2023-07-17 11:16:53 +0200

    Added Tailwind and first draft of AppBar

---
+++ frontend/sveltekit/src/lib/components/appBar.svelte
@@ -0,0 +1,24 @@
+  <div class="flex justify-between items-center bg-gray-900 px-4 py-2">
+    <div class="flex items-center">
+      <h1 class="text-white text-xl font-semibold">My App</h1>
+    </div>
+    <div class="flex items-center">
+      <div class="relative">
+        <button class="flex items-center text-white focus:outline-none">
+          <span class="text-sm font-medium">John Doe</span>
+          <svg class="h-4 w-4 ml-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path fill-rule="evenodd"
+              d="M10 12a2 2 0 100-4 2 2 0 000 4zm0 2a4 4 0 100-8 4 4 0 000 8zm6-4a6 6 0 11-12 0 6 6 0 0112 0z"
+              clip-rule="evenodd" />
+          </svg>
+        </button>
+        <div class="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg overflow-hidden z-10 invisible opacity-0">
+          <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Profile</a>
+          <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Preferences</a>
+          <button class="block w-full px-4 py-2 text-sm text-left text-gray-700 hover:bg-gray-100 focus:outline-none">
+            Logout
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
--- frontend/sveltekit/src/routes/+layout.svelte
+++ frontend/sveltekit/src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import "../app.css";
 	import { user } from '$lib';
 	export let data: PageData;
 
+	import AppBar from '$lib/components/appBar.svelte';
 	user.set(data.user);
 </script>
 
 <nav>
+	<AppBar></AppBar>
 	<ul>
 		<li>
 			<a href="/">Home</a>
